7ee52083c01ce17cd4e9c1806f5bd7f524f958a8
Tyrannous <nathan_hayman@hotmail.com>
2024-03-08 21:39:45 -0600

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,13 +6,9 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre> _______                                   _        _ 
- |__   __|                                 | |      | |/
-    | | _____      ___ __  _ __   ___  _ __| |_ __ _| |/
-    | |/ _ \ \ /\ / / '_ \| '_ \ / _ \| '__| __/ _` | |/
-    | | (_) \ V  V /| | | | |_) | (_) | |  | || (_| | |/
-    |_|\___/ \_/\_/ |_| |_| .__/ \___/|_|   \__\__,_|_|/
-                          | |                          
-                          |_|                          </pre> </div>
+<div class="custom-text-header"> <pre> 
+▀▀█▀▀ █▀▀█ █───█ █▀▀▄ ░█▀▀█ █▀▀█ █▀▀█ ▀▀█▀▀ █▀▀█ █── 
+─░█── █──█ █▄█▄█ █──█ ░█▄▄█ █──█ █▄▄▀ ──█── █▄▄█ █── 
+─░█── ▀▀▀▀ ─▀─▀─ ▀──▀ ░█─── ▀▀▀▀ ▀─▀▀ ──▀── ▀──▀ ▀▀▀  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
